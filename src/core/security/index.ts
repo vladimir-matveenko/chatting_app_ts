2eccefda26c7a83258d77011d801896e94acaf88
Vladimir Matveenko <vladimir.m@thecomity.com>
2026-07-04 16:13:01 +0300

    jwt service added

--- src/core/security/index.ts
+++ src/core/security/index.ts
@@ -1,2 +1,6 @@
 export * from "./password-hasher.js";
 export * from "./bcrypt-password-hasher.js";
+
+export * from "./jwt-payload.js";
+export * from "./jwt-service.js";
+export * from "./jsonwebtoken-jwt.service.js";
