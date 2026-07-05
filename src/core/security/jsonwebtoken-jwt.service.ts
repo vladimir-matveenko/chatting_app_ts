f02465a5ed55e1cf6324fa3d394aedb214c278f1
Vladimir Matveenko <vladimir.m@thecomity.com>
2026-07-05 13:33:12 +0300

    jwt service changed

--- src/core/security/jsonwebtoken-jwt.service.ts
+++ src/core/security/jsonwebtoken-jwt.service.ts
@@ -2,21 +2,31 @@ import jwt from "jsonwebtoken";
 
 import { env } from "../config/env.js";
 
-import type { JwtPayload } from "./jwt-payload.js";
-import type { JwtService } from "./jwt-service.js";
+import type { JwtPayload }
+    from "./jwt-payload.js";
 
-export class JwtServiceImpl implements JwtService {
+import type { JwtService }
+    from "./jwt-service.js";
+
+export class JwtServiceImpl
+    implements JwtService {
 
     signAccessToken(
         payload: JwtPayload,
     ): string {
 
         return jwt.sign(
+
             payload,
+
             env.jwtSecret,
+
             {
+
                 expiresIn: "15m",
+
             },
+
         );
 
     }
@@ -26,22 +36,45 @@ export class JwtServiceImpl implements JwtService {
     ): string {
 
         return jwt.sign(
+
             payload,
+
             env.jwtSecret,
+
             {
+
                 expiresIn: "30d",
+
             },
+
         );
 
     }
 
-    verify(
+    verifyAccessToken(
         token: string,
     ): JwtPayload {
 
         return jwt.verify(
+
             token,
+
             env.jwtSecret,
+
+        ) as JwtPayload;
+
+    }
+
+    verifyRefreshToken(
+        token: string,
+    ): JwtPayload {
+
+        return jwt.verify(
+
+            token,
+
+            env.jwtSecret,
+
         ) as JwtPayload;
 
     }
