f2d0d20bc9a12b0a01a65770214552112b1c4172
Vladimir Matveenko <vladimir.m@thecomity.com>
2026-07-08 19:52:57 +0300

    createChatsRouter fixed

--- src/features/chats/routes/chats.routes.ts
+++ src/features/chats/routes/chats.routes.ts
@@ -43,7 +43,7 @@ export function createChatsRouter(
 
     );
 
-    router.get(
+    router.post(
 
         "/",
 
@@ -51,7 +51,7 @@ export function createChatsRouter(
 
         asyncHandler(
 
-            controller.list.bind(
+            controller.create.bind(
 
                 controller,
 
