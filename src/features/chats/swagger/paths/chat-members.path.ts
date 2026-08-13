ef475bab6dcaafa573dde9541cee01126feb522b
Vladimir Matveenko <vladimir.m@thecomity.com>
2026-08-13 18:38:17 +0300

    swagger changed

--- src/features/chats/swagger/paths/chat-members.path.ts
+++ src/features/chats/swagger/paths/chat-members.path.ts
@@ -48,6 +48,41 @@ export const ChatMembersPath = {
   },
 
   "/chats/{id}/members/me": {
+    get: {
+      tags: ["Chats"],
+
+      summary: "Get current user from chat",
+
+      description: "Get current user from chat",
+
+      security: [
+        {
+          bearerAuth: [],
+        },
+      ],
+
+      parameters: [
+        {
+          name: "id",
+
+          in: "path",
+
+          required: true,
+
+          schema: {
+            type: "string",
+          },
+        },
+      ],
+
+      responses: {
+        200: okResponse("Chat members.", "#/components/schemas/ChatMember"),
+
+        401: unauthorizedResponse,
+
+        404: notFoundResponse,
+      },
+    },
     delete: {
       tags: ["Chats"],
 
